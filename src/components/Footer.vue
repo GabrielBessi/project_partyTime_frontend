a4c10caecd0234dca3f3402410cd3a990049f524
Gabriel Bessi <tecbessi@gmail.com>
2023-06-15 21:37:54 -0300

    feat<main>: Configuração inicial do projeto

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <h3>Party Time &copy; 2023</h3>
+    <p>O melhor lugar para compartilhar seus eventos</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: column;
+
+  background-color: #25282e;
+  height: 300px;
+
+  align-items: center;
+  justify-content: center;
+}
+
+footer h3 {
+  color: #fff;
+  margin-bottom: 15px;
+  font-weight: 300;
+  font-size: 1.6rem;
+}
+
+footer p {
+  color: #c1b696;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="nav">
+    <RouterLink to="/">
+      <img src="../../public/img/partytimelogo.png" alt="Party time" />
+    </RouterLink>
+    <h2 id="nav-title">Party Time</h2>
+    <div id="nav-links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">Entrar</router-link>
+      <router-link to="/about">Cadastrar</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+#nav {
+  display: flex;
+
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+
+  border-bottom: 1px solid #eee;
+}
+
+#nav a {
+  text-decoration: none;
+  margin-right: 15px;
+}
+
+#nav img {
+  width: 35px;
+}
+
+#nav-title {
+  font-size: 3rem;
+  font-weight: 300;
+}
+
+/* #logo-container,
+#nav-links {
+  width: 400px;
+} */
+
+#nav-links {
+  display: flex;
+  justify-content: flex-end;
+}
+
+button {
+  background-color: #fff;
+  border: none;
+
+  font-size: 16px;
+  cursor: pointer;
+}
+
+bottom:hover {
+  color: #c1b696;
+}
+</style>
