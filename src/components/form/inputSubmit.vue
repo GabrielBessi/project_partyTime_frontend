276733482b31e6ccc0213e7c660fc5403405a4ea
Gabriel Bessi <tecbessi@gmail.com>
2023-06-22 20:54:26 -0300

    feat<view-register>: Criação do componente submit

--- src/components/form/inputSubmit.vue
+++ src/components/form/inputSubmit.vue
@@ -11,4 +11,21 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+input {
+  width: 200px;
+  margin: 0 auto;
+  text-transform: uppercase;
+  color: #fff;
+  background-color: #25282e;
+  border: 0;
+  border-radius: 10px;
+  padding: 12px;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+input:hover {
+  background-color: #141619;
+}
+</style>
